f04242538c553a63e0aeb8039f4e1f45c09d8cbe
huanNguyen2204 <huan.nc2204@sinhvien.hoasen.edu.vn>
2023-11-25 12:25:19 +0700

    socket

---
+++ Chapter12-WebSocket/socket/index.js
@@ -0,0 +1,24 @@
+
+
+// const connection = new WebSocket('ws://example.com/test');
+
+// connection.onopen = (_event) => {
+//   console.log("Connection opened");
+// };
+
+// connection.onerror = (_error) => {
+//   console.log(`WebSocket Error: ${_error}`);
+// };
+
+// connection.onclose = (_event) => {
+//   console.log("Connection closed");
+// };
+
+// connection.onmessage = (_event) => {
+//   try {
+//     const data = JSON.parse(_event.data);
+//     console.log(data);
+//   } catch (error) {
+//     console.log("Error parsing");
+//   } 
+// }
